034201e5836ca666ec39940f38b24bb5be183272
DuraBull <111558099+DuraBull@users.noreply.github.com>
2023-10-09 00:31:09 -0400

    +live pipeline

---
+++ live/js/ham.js
@@ -0,0 +1,9 @@
+let brandHam = document.querySelector(".hamburger");
+let brandNav = document.querySelector(".main-nav")
+brandHam.addEventListener("click", openNav);
+
+console.log(brandHam);
+
+function openNav() {
+    brandNav.classList.toggle("active");
+}
