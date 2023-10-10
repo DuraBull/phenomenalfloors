f1e40323c9ea24f11a9ef532912ab7c23305c07f
DuraBull <111558099+DuraBull@users.noreply.github.com>
2023-10-09 23:08:53 -0400

    desktop layout init

--- Redesign v1.0/js/ham.js
+++ Redesign v1.0/js/ham.js
@@ -1,9 +1,9 @@
 let brandHam = document.querySelector(".hamburger");
-let brandNav = document.querySelector(".main-nav")
+let brandNav = document.querySelector(".main-nav");
 brandHam.addEventListener("click", openNav);
 
 console.log(brandHam);
 
 function openNav() {
-    brandNav.classList.toggle("active");
+  brandNav.classList.toggle("active");
 }
